6c195a7cf41ed00b1c0d4ecb3efc5ad1991c4220
Jurgen Kluft <jurgen@virtuos.com.cn>
2011-01-11 14:23:33 +0800

    *: added an easy install batch file *: renamed Category to ProjectGroup

---
+++ install.bat
@@ -0,0 +1,4 @@
+xcopy source\main\resources\tasks\MSBuild.XCode\MSBuild.XCode\bin\Release\MSBuild.XCode.dll D:\PACKAGE_REPO\com\virtuos\xcode\publish /R /Y /Q
+xcopy source\main\resources\tasks\MSBuild.XCode\MSBuild.XCode\References\Ionic.Zip.dll D:\PACKAGE_REPO\com\virtuos\xcode\publish /R /Y /Q
+xcopy source\main\resources\*.* D:\PACKAGE_REPO\com\virtuos\xcode\publish /R /Y /Q
+xcopy source\main\resources\templates\*.* D:\PACKAGE_REPO\com\virtuos\xcode\publish\templates /R /Y /Q
