ddbc48e07c88f57bedf5cc2e15d160ae8407f272
Jurgen Kluft <jurgen@virtuos.com.cn>
2011-01-17 16:36:28 +0800

    +: added a 'release' batch files which will release a new version to the server

---
+++ release.bat
@@ -0,0 +1,4 @@
+xcopy source\main\resources\tasks\MSBuild.XCode\MSBuild.XCode\bin\Release\MSBuild.XCode.dll \\cnshasap2\Hg_Repo\PACKAGE_REPO\com\virtuos\xcode\publish /R /Y /Q
+xcopy source\main\resources\tasks\MSBuild.XCode\MSBuild.XCode\References\Ionic.Zip.dll \\cnshasap2\Hg_Repo\PACKAGE_REPO\com\virtuos\xcode\publish /R /Y /Q
+xcopy source\main\resources\*.* \\cnshasap2\Hg_Repo\PACKAGE_REPO\com\virtuos\xcode\publish /R /Y /Q
+xcopy source\main\resources\templates\*.* \\cnshasap2\Hg_Repo\PACKAGE_REPO\com\virtuos\xcode\publish\templates /R /Y /Q
