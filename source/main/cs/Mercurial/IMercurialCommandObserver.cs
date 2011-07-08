f5e6fbebebfb98e58ad8b90a991ae7c35bd7e94c
Jurgen Kluft <jurgen@virtuos.com.cn>
2011-07-08 11:28:17 +0800

    *: XCode now use Hg Clone to get the XCode instead of FileCopy, this enables us to use Hg Clone over HTTP.

---
+++ source/main/cs/Mercurial/IMercurialCommandObserver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Mercurial
+{
+    /// <summary>
+    /// This interface must be implemented by an object that will act as an observer
+    /// of events related to Mercurial command execution.
+    /// </summary>
+    public interface IMercurialCommandObserver
+    {
+        /// <summary>
+        /// This method will be called once for each line of normal output from the command. Note that this method will be called on
+        /// a different thread than the thread that called the <see cref="Client.Execute(string,IMercurialCommand)"/> method.
+        /// </summary>
+        void Output(string line);
+
+        /// <summary>
+        /// This method will be called once for each line of error output from the command. Note that this method will be called on
+        /// a different thread than the thread that called the <see cref="Client.Execute(string,IMercurialCommand)"/> method.
+        /// </summary>
+        void ErrorOutput(string line);
+
+        /// <summary>
+        /// This method will be called before the command starts executing.
+        /// </summary>
+        void Executing(string command, string arguments);
+
+        /// <summary>
+        /// This method will be called after the command has terminated (either timed out or completed by itself.)
+        /// </summary>
+        void Executed(string command, string arguments, int exitCode, string output, string errorOutput);
+    }
+}
---
+++ source/main/cs/Mercurial/ShowConfigCommand.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Mercurial
+{
+    /// <summary>
+    /// This class implements the "hg showconfig" command (http://www.selenic.com/mercurial/hg.1.html#showconfig)
+    /// </summary>
+    public sealed class ShowConfigCommand : CommandBase<ShowConfigCommand>, IMercurialCommand<IEnumerable<ConfigurationEntry>>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShowConfigCommand"/> class.
+        /// </summary>
+        public ShowConfigCommand()
+            : base("showconfig")
+        {
+        }
+
+        #region IMercurialCommand<IEnumerable<ConfigurationEntry>> Members
+
+        /// <summary>
+        /// The result from the command line execution, as an appropriately typed
+        /// value.
+        /// </summary>
+        public IEnumerable<ConfigurationEntry> Result
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// This method should parse and store the appropriate execution result output
+        /// according to the type of data the command line client would return for
+        /// the command.
+        /// </summary>
+        /// <param name="exitCode">
+        /// The exit code from executing the command line client.
+        /// </param>
+        /// <param name="standardOutput">
+        /// The standard output from executing the command line client.
+        /// </param>
+        /// <remarks>
+        /// Note that as long as you descend from <see cref="CommandBase{T}"/> you're not required to call
+        /// the base method at all.
+        /// </remarks>
+        protected override void ParseStandardOutputForResults(int exitCode, string standardOutput)
+        {
+            var re = new Regex(@"^(?<section>[^.]+)\.(?<name>[^=]+)=(?<value>.*)$", RegexOptions.None);
+
+            var entries = new List<ConfigurationEntry>();
+            using (var reader = new StringReader(standardOutput))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Match ma = re.Match(line);
+                    if (ma.Success)
+                    {
+                        entries.Add(new ConfigurationEntry(ma.Groups["section"].Value.Trim(), ma.Groups["name"].Value.Trim(),
+                            ma.Groups["value"].Value.Trim()));
+                    }
+                }
+            }
+            Result = entries;
+        }
+    }
+}
