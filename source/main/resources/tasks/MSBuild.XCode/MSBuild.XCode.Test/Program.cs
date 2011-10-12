fdc2050e927a39c64c4f487c144eb7b58bd71d11
Jurgen Kluft <jurgen@virtuos.com.cn>
2011-10-12 18:16:42 +0800

    *: DirectoryInfo.Remove now uses the shell (cmd) to delete a dir, workaround for the MAX_PATH issue *: DirectoryInfo.ReadOnly also uses the shell to set attributes of all subdirs and files, also workaround for MAX_PATH issue

--- source/main/resources/tasks/MSBuild.XCode/MSBuild.XCode.Test/Program.cs
+++ source/main/resources/tasks/MSBuild.XCode/MSBuild.XCode.Test/Program.cs
@@ -19,7 +19,7 @@ namespace MSBuild.XCode.Test
         {
             Loggy.ToConsole = true;
 
-            string name = "xthread";
+            string name = "xmemory";
 
             // home (hp laptop)
             //RemoteRepoDir = @"db::server=127.0.0.1;port=3306;database=xcode;uid=root;password=p1|fs::D:\PACKAGE_REPO_TEST\";
@@ -38,7 +38,7 @@ namespace MSBuild.XCode.Test
 
             if (Update("1.1.0.0"))
             {
-                string platform = "Win32";
+                string platform = "WII";
                 Construct(name, platform);
                 Create(name, platform);
                 Install(name, platform);
