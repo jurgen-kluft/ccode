f724b5cfd7a27f1612c7adacf5439360af7728a6
jurgen.kluft <jurgen.kluft@gmail.com>
2024-06-16 21:25:10 +0800

    * axe boolean, remove the maps

--- axe/axe-boolean.go
+++ axe/axe-boolean.go
@@ -1,25 +1,25 @@
 package axe
 
-var BooleanToYesNo = map[bool]string{
-	true:  "YES",
-	false: "NO",
-}
-
-var BooleanToTrueFalse = map[bool]string{
-	true:  "true",
-	false: "false",
-}
-
-var BooleanTo01 = map[bool]int{
-	true:  1,
-	false: 0,
-}
-
 type Boolean bool
 
-func (b Boolean) YesNo() string     { return BooleanToYesNo[bool(b)] }
-func (b Boolean) TrueFalse() string { return BooleanToTrueFalse[bool(b)] }
-func (b Boolean) Int0or1() int      { return BooleanTo01[bool(b)] }
-func (b Boolean) Bool() bool        { return bool(b) }
-func (b Boolean) Set(v bool)        { b = Boolean(v) }
-func (b Boolean) Get() bool         { return bool(b) }
+func (b Boolean) YesNo() string {
+	if b.Bool() {
+		return "YES"
+	}
+	return "NO"
+}
+func (b Boolean) TrueFalse() string {
+	if b.Bool() {
+		return "true"
+	}
+	return "false"
+}
+func (b Boolean) Int0or1() int {
+	if b.Bool() {
+		return 1
+	}
+	return 0
+}
+func (b Boolean) Bool() bool { return bool(b) }
+func (b Boolean) Set(v bool) { b = Boolean(v) }
+func (b Boolean) Get() bool  { return bool(b) }
